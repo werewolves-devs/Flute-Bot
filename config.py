73745e7f159f75a991b212095f127dc90e2d9074
Randium <33968886+Randium@users.noreply.github.com>
2018-06-09 12:04:22 +0200

    Added config file
    Note the universal prefix that will be used by and accepted for all bots.

---
+++ config.py
@@ -0,0 +1,19 @@
+# Discord API / Login settings
+TOKEN = 'put_token_here'
+
+# Prefixes
+prefix = '!'
+universal_prefix = '#003-88-6521#'
+
+# List of specific channels
+welcome_channel = "0" #TODO: Channel where the bot says it's gone online.
+bot_spam = "1" #TODO
+flute = "2" #TODO
+victim = "3" #TODO
+
+# Tutorial
+tutorial = "Hey there, buddy! I am your buddy, the Flute Bot! I will record anything said in <#" + victim + ">,"
+tutorial += " so you can spy on your victims!\n Also, if you ever need to tell them something, you can always"
+tutorial += " type `" + prefix + "whisper <message>` to whisper a message to our victims.\n"
+tutorial += "It is up to you to decide how you will play your game. Filled with riddles or silent in the dark."
+tutorial += " Either way, I'll be here to help!"
